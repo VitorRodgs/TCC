62547271aee076261820daf988ed9fe9395b66f7
VitorRodgs <vitor-pessoa@hotmail.com>
2023-06-20 01:55:11 -0300

    Atualização da Página de Criação de Questionário

--- frontend/src/components/VerificaCampo.js
+++ frontend/src/components/VerificaCampo.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import CampoForm from './CampoForm';
+import './VerificaCampo.css';
 
 const VerificaCampo = () => {
   const [campos, setCampos] = useState([]);
   const [nome, setNome] = useState('');
   const [tipo, setTipo] = useState('');
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [link, setLink] = useState('');
 
   useEffect(() => {
     getCampos();
@@ -16,7 +20,7 @@ const VerificaCampo = () => {
   const getOpcoes = (campo) => {
     if (campo && campo.opcoes) {
       try {
-        const opcoesArray = campo.opcoes; // Remover o JSON.parse
+        const opcoesArray = campo.opcoes;
         if (Array.isArray(opcoesArray) && opcoesArray.length > 0) {
           return opcoesArray;
         }
@@ -26,13 +30,11 @@ const VerificaCampo = () => {
     }
     return [];
   };
-  
 
   const getCampos = async () => {
     try {
       const response = await axios.get('http://localhost:8081/campos');
       setCampos(response.data);
-      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -41,20 +43,22 @@ const VerificaCampo = () => {
   const saveForm = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:8081/formularios', {
+      const response = await axios.post('http://localhost:8081/formularios', {
         nome: nome,
         tipo: tipo
       });
-      alert('Formulário criado com sucesso!'); // Exemplo de mensagem de sucesso
+      const formId = response.data.id;
+      const formLink = `https://www.exemplo.com/formulario/${formId}`;
+      setLink(formLink);
+      setPopupOpen(true);
     } catch (error) {
       console.error(error);
-      alert('Ocorreu um erro ao criar o formulário. Por favor, tente novamente.'); // Exemplo de mensagem de erro
+      alert('Ocorreu um erro ao criar o formulário. Por favor, tente novamente.');
     }
   };
 
   const renderOpcoes = (opcoes, id, nome) => {
     const opcoesArray = JSON.parse(opcoes);
-    console.log(opcoesArray);
     return opcoesArray.map((opcao, index) => (
       <div key={index}>
         <input id={`${id}-opcao${index}`} className="input" name={nome} type="checkbox" value={opcao} />
@@ -64,13 +68,13 @@ const VerificaCampo = () => {
   };
 
   const verifica = (nome, tipo, id, opcoes) => {
-    var Texto = 'Texto';
-    var Numero = 'Numero';
-    var Multipla = 'SelecaoMultipla';
-    var Unica = 'EscolhaUnica';
-    var Data = 'Data';
-    var Selecao = 'CaixaSelecao';
-  
+    const Texto = 'Texto';
+    const Numero = 'Numero';
+    const Multipla = 'SelecaoMultipla';
+    const Unica = 'EscolhaUnica';
+    const Data = 'Data';
+    const Selecao = 'CaixaSelecao';
+
     if (tipo === Texto) {
       return (
         <div>
@@ -90,7 +94,6 @@ const VerificaCampo = () => {
         </div>
       );
     } else if (tipo === Multipla) {
-      console.log(opcoes);
       return (
         <div>
           <label className="label" htmlFor={id}>{nome}</label>
@@ -129,14 +132,25 @@ const VerificaCampo = () => {
       );
     }
   };
-  
+
+  const copyLink = () => {
+    const linkInput = document.getElementById('linkInput');
+    linkInput.select();
+    linkInput.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    setCopied(true);
+  };
+
+  const resetCopied = () => {
+    setCopied(false);
+  };
 
   if (campos.length === 0) {
-    return <p>Carregando...</p>; // Exibir uma mensagem de carregamento enquanto os dados são buscados
+    return <p>Carregando...</p>;
   }
 
   return (
-    <form id='1' onSubmit={saveForm}>
+    <form id="1" onSubmit={saveForm}>
       {campos.map((campo, index) => (
         <div key={campo.id}>
           {verifica(campo.nome, campo.tipo, campo.id, getOpcoes(campo))}
@@ -144,10 +158,52 @@ const VerificaCampo = () => {
       ))}
       <br />
       <div>
-        <button type='submit' className='button is-info' form='1'>Criar Formulário</button>
+        <button type="submit" className="button is-info" form="1">
+          Criar Questionário
+        </button>
         <br />
-        <Link to="/" className="button is-info mt-2">Voltar</Link>
+        <Link to="/" className="button is-info mt-2">
+          Voltar
+        </Link>
       </div>
+
+      {popupOpen && (
+        <div className="modal is-active">
+          <div className="modal-background"></div>
+          <div className="modal-card popup-content">
+            <header className="modal-card-head">
+              <p className="modal-card-title">Link do Questionário</p>
+              <button
+                className="delete"
+                aria-label="close"
+                onClick={() => {
+                  setPopupOpen(false);
+                  resetCopied();
+                }}
+              ></button>
+            </header>
+            <section className="modal-card-body">
+              <div className="field">
+                <label className="label">Link:</label>
+                <div className="control">
+                  <input
+                    id="linkInput"
+                    className="input"
+                    type="text"
+                    value={link}
+                    readOnly
+                  />
+                </div>
+              </div>
+            </section>
+            <footer className="modal-card-foot">
+              <button className="button is-small" onClick={copyLink}>
+                {copied ? 'Link Copiado!' : 'Copiar Link'}
+              </button>
+            </footer>
+          </div>
+        </div>
+      )}
     </form>
   );
 };
